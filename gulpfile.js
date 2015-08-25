904b2b8fb14539fbef69bd65b94c39c93d50db1f
Barton Hammond <bartonhammond@gmail.com>
2015-08-25 13:38:25 -0500

    Add event for calendar

--- gulpfile.js
+++ gulpfile.js
@@ -58,9 +58,10 @@ gulp.task('elements', function () {
 // Lint JavaScript
 gulp.task('jshint', function () {
   return gulp.src([
-      'app/scripts/**/*.js',
-      'app/elements/**/*.js',
-      'app/elements/**/*.html'
+    '!app/elements/**/*.min.js',
+    'app/scripts/**/*.js',
+    'app/elements/**/*.js',
+    'app/elements/**/*.html'
     ])
     .pipe(reload({stream: true, once: true}))
     .pipe($.jshint.extract()) // Extract JS from .html files
